a98a75b028ec99647e7d7acde9e611e4cbb8fd1a
rohitlingarker <rohitlingerker@gmail.com>
2022-11-26 23:44:59 +0530

    l7

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.set("view engine","ejs");
 
 app.get("/", async function (request, response) {
+  
   const allTodos = await Todo.getTodos();
   if(request.accepts('html')){
     response.render('index',{allTodos})
