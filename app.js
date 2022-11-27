9bfdc95c94ead9b8550e7ab9f61bf527d9165102
rohitlingarker <rohitlingerker@gmail.com>
2022-11-27 10:13:07 +0530

    added delete functionality

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.get("/", async function (request, response) {
     });
   } else {
     response.json({
-      allTodos,
       overdues,
       itemsDueToday,
       itemsDueLater,
@@ -86,40 +85,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
   // FILL IN YOUR CODE HERE
-  const todo = await Todo.findByPk(request.params.id);
-
-  if (todo) {
-    await todo
-      .destroy()
-      .then((result) => {
-        response.send(true);
-      })
-      .catch((err) => {
-        response.send(false);
-      });
-  } else {
-    response.send(false);
+  try {
+    await Todo.remove(request.params.id)
+    return response.json({ success:true})
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error)
   }
-  //----------------------------------------------------------
-  // try {
-  //   await Todo.destroy({
-  //     where: { id: request.params.id },
-  //   })
-  //     .then((result) => {
-  //       response.send(true);
-  //     })
-  //     .catch((err) => {
-  //       response.send(false);
-  //     });
-  // } catch (error) {
-  //   response.status(422).json(error);
-  // }
-
-  //-----------------------------------------------------------
-
-  // First, we have to query our database to delete a Todo by ID.
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
 });
 
 module.exports = app;
--- models/todo.js
+++ models/todo.js
@@ -15,6 +15,13 @@ module.exports = (sequelize, DataTypes) => {
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
+    static async remove(id){
+      return this.destroy({
+        where:{
+          id,
+        }
+      })
+    }
 
     static getTodos() {
       return this.findAll();
