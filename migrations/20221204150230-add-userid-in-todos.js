48d7b382c6ba7e902279da978d734b9336d44d50
rohitlingarker <rohitlingerker@gmail.com>
2022-12-04 22:07:45 +0530

    rectifying migration

--- migrations/20221204150230-add-userid-in-todos.js
+++ migrations/20221204150230-add-userid-in-todos.js
@@ -3,19 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await queryInterface.addColumn("Todos", "userId", {
-    //   type: Sequelize.DataTypes.INTEGER,
-    // });
+    await queryInterface.addColumn("Todos", "userId", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
 
-    // await queryInterface.addConstraint("Todos", {
-    //   fields: ["userId"],
-    //   type: "foreign key",
-    //   references: {
-    //     table: "Users",
-    //     field: "id",
-    //   },
-    // });
-    await queryInterface.removeColumn("Todos", "userId");
+    await queryInterface.addConstraint("Todos", {
+      fields: ["userId"],
+      type: "foreign key",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+    });
     /**
      * Add altering commands here.
      *
