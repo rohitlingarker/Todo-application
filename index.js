e556116399cc46c3012662fb5e710236749a3418
rohitlingarker <rohitlingerker@gmail.com>
2024-04-03 00:04:16 +0530

    deploying with pm2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PORT : process.env.DEV_DB_PORT || 3000;
 app.listen(PORT, function () {
-  console.log('Started express server at port:'+PORT)
+  console.log('started express server at port:'+PORT)
 })
 
